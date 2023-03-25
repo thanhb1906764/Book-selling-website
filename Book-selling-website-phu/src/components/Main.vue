c8dd472bbccf784d315bd4a11e5d4e7b02df64c8
thanb1906764 <thanhb1906764@student.ctu.edu.vn>
2023-03-25 18:20:12 +0700

    fix git

--- Book-selling-website-phu/src/components/Main.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    Mainnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
-</template>
--- backend/app/config/index.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    app: {
-        port: process.env.PORT || 3000
-    },
-    db: {
-        uri: process.env.MONGODB_URI || "mongodb+srv://mongo:mongo@clusternienluan.jqgy2ey.mongodb.net/testDataHere?retryWrites=true&w=majority"
-    }
-};
-module.exports = config;
