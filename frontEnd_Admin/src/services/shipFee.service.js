8b570d12e2f3c35362f0285425a072351a986779
thanb1906764 <thanhb1906764@student.ctu.edu.vn>
2023-03-27 08:52:30 +0700

    add shipFee frontend

---
+++ frontEnd_Admin/src/services/shipFee.service.js
@@ -0,0 +1,31 @@
+import createApiClient from "./api.service";
+
+class ShipFeeService {
+    constructor(baseUrl = "/api/ShipFee") {
+        this.api = createApiClient(baseUrl);
+    }
+
+    async getAll(data) {
+        return (await this.api.get("/", data)).data;
+    }
+
+    async create(data) {
+        return (await this.api.post("/", data)).data;
+    }
+
+    async deleteAll(data) {
+        return (await this.api.delete("/"), data).data;
+    }
+
+    async get(id) {
+        return (await this.api.get(`/${id}`)).data;
+    }
+
+    async updateShipFee(id, data) {
+        return (await this.api.put(`/${id}`, data)).data;
+    }
+
+    async delete(id) {
+        return (await this.api.delete(`${id}`)).data;
+    }
+}
