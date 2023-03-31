99c496a5fd97fae771ffa7b7f3b588177bb3e057
thanb1906764 <thanhb1906764@student.ctu.edu.vn>
2023-03-31 12:36:35 +0700

    fix error push b1906764

--- frontEnd_Admin/src/main.js
+++ frontEnd_Admin/src/main.js
@@ -14,17 +14,10 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
 import router from "./routers";
-
-
-
 const vuetify = createVuetify({
     components,
     directives,
 })
-
-
 const app = createApp(App)
-
 app.use(createPinia()).use(Vue3Toastify).use(vuetify).use(router)
-
 app.mount('#app')
