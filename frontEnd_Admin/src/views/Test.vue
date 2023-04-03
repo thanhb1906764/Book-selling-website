fc95a1030528dd849ffbd7028a0a78f7647af23b
Kizuna Yuuki <kizunayuuki@Kizunas-iMac.local>
2023-04-03 19:22:51 +0700

    paypal

--- frontEnd_Admin/src/views/Test.vue
+++ frontEnd_Admin/src/views/Test.vue
@@ -1,91 +1,28 @@
-<template>
-    <div class="toast-container position-static">
-        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="toast-header">
-                <img src="..." class="rounded me-2" alt="...">
-                <strong class="me-auto">Bootstrap</strong>
-                <small class="text-muted">just now</small>
-                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-            </div>
-            <div class="toast-body">
-                See? Just like this.
-            </div>
-        </div>
-
-        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="toast-header">
-                <img src="..." class="rounded me-2" alt="...">
-                <strong class="me-auto">Bootstrap</strong>
-                <small class="text-muted">2 seconds ago</small>
-                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-            </div>
-            <div class="toast-body">
-                Heads up, toasts will stack automatically
-            </div>
-        </div>
-    </div>
-    <!-- <div>{{ Books }}</div> -->
-    <!-- <router-link :to="{
-        name: 'BookDetails',
-        params: { id: '123' },
-    }">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>
-            </div>
-        </div>
-    </router-link> -->
+<template >
+    <div class="text-center" id="your-container-element" style="margin: auto; width: 20%;"></div>
 </template>
 
 <script>
-import BooksService from '@/services/books.service'
-import CartsService from '../services/carts.service';
-import { useDataStore } from '../stores/dataStores';
-import axios from 'axios'
+import { loadScript } from "@paypal/paypal-js";
 
 export default {
-    components: {
+    // reder button paypal 
+    async mounted() {
+        let paypal;
 
-    },
-    data() {
-        return {
-            Books: [],
-            Cookies: [],
-            id: '1',
-            quan: 12
+        try {
+            paypal = await loadScript({ "client-id": "ASrlSiWXjfgu4XajnAAVyRgMW0MxI7xaZUuyPOcSlUBbgNrPd6_NpSJO-KTmo-0unicA1nGyeEr_hHPZ" });
+        } catch (error) {
+            console.error("failed to load the PayPal JS SDK script", error);
         }
-    },
-    methods: {
-        ToastFunction() {
-            const toastTrigger = document.getElementById('liveToastBtn')
-            const toastLiveExample = document.getElementById('liveToast')
-            const toast = new bootstrap.Toast(toastLiveExample)
-            toast.show()
-        },
-        async retrieveBook() {
+
+        if (paypal) {
             try {
-                axios
-                    .get(`http://localhost:3000/cookies/set/${this.id}/${this.quan}`, {
-                        withCredentials: true
-                    })
-                    .then((response) => {
-                        this.Cookies = response.data
-                        console.log(response)
-                    })
-                // // this.Cookies = await CartsService.getAll();
-                // // this.Cookies = await CookiesService.get()
-                // this.Cookies = await CartsService.createCookie(this.id, this.quan);
-                // // useDataStore().setBooks(this.Books)
-                console.log(this.Cookies)
+                await paypal.Buttons().render("#your-container-element");
             } catch (error) {
-                console.log(error);
+                console.error("failed to render the PayPal Buttons", error);
             }
         }
-    }, mounted() {
-
-        this.retrieveBook()
-    }
-}
+    },
+};
 </script>
