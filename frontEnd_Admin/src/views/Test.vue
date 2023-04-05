aa7f764c58f6adbd84b6d07e19aa639a6b255f01
Kizuna Yuuki <kizunayuuki@Kizunas-iMac.local>
2023-04-05 18:39:54 +0700

    paypal

--- frontEnd_Admin/src/views/Test.vue
+++ frontEnd_Admin/src/views/Test.vue
@@ -9,16 +9,67 @@ export default {
     // reder button paypal 
     async mounted() {
         let paypal;
-
+        // Load PayPal JS SDK
         try {
-            paypal = await loadScript({ "client-id": "ASrlSiWXjfgu4XajnAAVyRgMW0MxI7xaZUuyPOcSlUBbgNrPd6_NpSJO-KTmo-0unicA1nGyeEr_hHPZ" });
+            paypal = await loadScript({ "client-id": "AQKDZ7Sr1AS-DGFMAHFDd3m8BLPDuCL8v8zjU9iesZO2H0zk8g2Dh1D-9gO-Uq3wNmIIBuxKfqJ5ADCT" });
         } catch (error) {
             console.error("failed to load the PayPal JS SDK script", error);
         }
-
+        // Handler click button
         if (paypal) {
             try {
-                await paypal.Buttons().render("#your-container-element");
+                await paypal
+                    .Buttons({
+                        // Sets up the transaction when a payment button is clicked
+                        createOrder: async function () {
+                            const response = await fetch("http://localhost:3000/create-paypal-order", {
+                                method: "post",
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                                // use the "body" param to optionally pass additional order information
+                                // like product skus and quantities
+                                body: JSON.stringify({
+                                    cart: [
+                                        {
+                                            sku: "YOUR_PRODUCT_STOCK_KEEPING_UNIT",
+                                            quantity: "YOUR_PRODUCT_QUANTITY",
+                                        },
+                                    ],
+                                }),
+                            });
+                            const order = await response.json();
+                            return order.id;
+                        },
+                        // Finalize the transaction after payer approval
+                        onApprove: async function (data) {
+                            const response = await fetch("http://localhost:3000/capture-paypal-order", {
+                                method: "post",
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                                body: JSON.stringify({
+                                    orderID: data.orderID,
+                                }),
+                            });
+                            const orderData = await response.json();
+                            // Successful capture! For dev/demo purposes:
+                            console.log(
+                                "Capture result",
+                                orderData,
+                                JSON.stringify(orderData, null, 2)
+                            );
+                            const transaction = orderData.purchase_units[0].payments.captures[0];
+                            alert(
+                                "Transaction " +
+                                transaction.status +
+                                ": " +
+                                transaction.id +
+                                "\n\nSee console for all available details"
+                            );
+                        },
+                    })
+                    .render("#your-container-element");
             } catch (error) {
                 console.error("failed to render the PayPal Buttons", error);
             }
